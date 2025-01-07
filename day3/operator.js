a215d9a04a9985f13349648604ed68feb402a2c5
Ashwithareddy484 <ashwithareddy9435@gmail.com>
2025-01-07 14:12:18 +0530

    pulling

--- day3/operator.js
+++ day3/operator.js
@@ -1,12 +1,13 @@
 console.log(5+5);
 console.log(100+1000);
 console.log(10+"100");
+console.log(10+20+"Hi");
+console.log("hi"+10+50);
+console.log("hi"+(10+50));
 
-
-
-
-
-
+console.log(10-5);
+console.log("10"-5);//! typecasting
+console.log("10"-"2");
 
 
 console.log(5*5);
