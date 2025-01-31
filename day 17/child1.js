d6b96a477093f2466c7a1c83d76c7f18c18d98cb
Ashwithareddy484 <ashwithareddy9435@gmail.com>
2025-01-31 13:02:35 +0530

    pushed

---
+++ day 17/child1.js
@@ -0,0 +1,43 @@
+// // let x = 10;
+
+// // export default x;
+
+// let userDetail={
+//   name: "ashwitha",
+//   profession:"CR"
+// }
+// export default userDetail;
+
+// // !named export 
+
+// let x = 10;
+
+// const user="Preethi";
+// const suers =["harika", "shannu", "sachin", "sathwik", "sonu"];
+
+// const obj={
+//   name:"sam",
+//   company:"ExcelR"
+// }
+
+// //!named export
+// export{
+//   x,
+//   user,
+//   users,
+//   obj
+// }
+export let x = 10;
+
+export const user="Ashwitha";
+
+export const users = ["preethi","shannu", "hanvitha", "sachin", "bhanu"]
+
+export const obj={
+  name:"sam",
+  comapny:"ExcelR"
+}
+
+let college="MallaReddy University";
+
+export default college;
